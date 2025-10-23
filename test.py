574b760fb2f4edaea872c502a8f932b3238a5f95
Moises Alvarez <mfalvare@espol.edu.ec>
2025-10-23 16:08:12 -0500

    coreccion de errores

--- test.py
+++ test.py
@@ -5,15 +5,17 @@ class student:
         self.grades = []
         self.is_passed = "NO"
         self.honor = "?"
+        self.letter ="?"
 
     def addGrades(self, grade):
         if(grade>0 and grade <= 100):
             self.grades.append(grade)
-        
+        else:
+            print("Can´t add negative grades")
 
-    def calcaverage(self):
+    def calcAverage(self):
         t = 0
-        for x in self.gradez:
+        for x in self.grades:
             t += x
         len_grades = len(self.grades)
         if (len_grades)>0:
@@ -26,22 +28,22 @@ class student:
             self.honor = "yep"
 
     def deleteGrade(self, index):
-        del self.gradez[index]
+        del self.grades[index]
 
     def report(self):  # broken format
         print("ID: " + self.id)
         print("Name is: " + self.name)
-        print("Grades Count: " + len(self.gradez))
+        print(f"Grades Count: {len(self.grades)}")
         print("Final Grade = " + self.letter)
 
 
 def startrun():
-    a = student("x", "")
+    a = student("x", "fer")
     a.addGrades(100)
    # a.addGrades("Fifty")  # broken
-    a.calcaverage()
+    a.calcAverage()
     a.checkHonor()
-    a.deleteGrade(5)  # IndexError
+    #a.deleteGrade(5)  # IndexError
     a.report()
 
 
